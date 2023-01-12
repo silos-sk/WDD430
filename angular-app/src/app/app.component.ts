47624712dfd28057498e8edba0f374315c35b318
Shaira <sil21019@byui.edu>
2023-01-12 12:41:15 +0000

    Completed Assignment - Directives

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -8,10 +8,18 @@ import { Component } from '@angular/core';
   h3 {
     color: dodgerblue;
   }
+  .white{
+    color: white;
+  }
   `]
 })
 export class AppComponent {
   userName = '';
+  content = 'Secret Password';
+  displayStatus = false; 
+  count = 0
+  countArr: number[] = [];
+  aboveFive = false;
 
   constructor(){
 
@@ -19,5 +27,15 @@ export class AppComponent {
   ngOnInit() {
     
   }
-  
+  toggleContent(){
+   this.count += 1;
+   this.countArr.push(this.count);
+   return this.count % 2 === 0 ? this.displayStatus = true : this.displayStatus = false;
+   
+  }
+
+  getColor(){
+    // return this.count >= 5 ? this.aboveFive = true : this.aboveFive = false
+    return this.count >= 5 ? 'blue' : 'white'
+  }
 }
