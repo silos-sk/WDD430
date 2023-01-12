35bb508c29ec1172db7f117e2e888c6a4a53156d
Shaira <sil21019@byui.edu>
2023-01-12 09:47:04 +0000

    assignment completed - component basics

---
+++ angular-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '.app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  // styleUrls: ['./warning-alert.component.css']
+  styles: [`
+    p {
+      color: red;
+    }
+  `]
+})
+export class WarningAlertComponent {
+
+}
