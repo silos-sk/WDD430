29e535253323140265207eac0a2e27f5223b7948
Shaira <sil21019@byui.edu>
2023-01-13 23:42:14 +0000

    shopping list content updated

--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients = []
 
+  constructor(){ }
+
+  ngOnInit(){ }
 }
