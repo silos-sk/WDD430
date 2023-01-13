5a56bd6b7081927c01a87c2c7be846e09b39eae9
Shaira <sil21019@byui.edu>
2023-01-13 23:24:41 +0000

    bootstrap 3 reinstalled - navbar edited

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] =[
+  new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.magimix.com/files/rec_82280/bouillon%20(3)_photo.jpg')
+]
+  constructor(){
 
+  }
+
+  ngOnInit(){
+
+  }
 }
