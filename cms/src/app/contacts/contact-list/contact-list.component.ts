8415fe7a45b91a888891b9c8e852e7d8e0384ed3
Shaira <sil21019@byui.edu>
2023-01-14 01:47:11 +0000

    display contactlist component

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent {
-
+    contacts: Contact[] = [
+      new Contact(1, 
+      "R. Kent Jackson", 
+      "jacksonk@byui.edu",
+      208-496-3771,
+      "../../assets/images/jacksonk.jpg", 
+      "null"
+      ),
+      new Contact(2, 
+        "Rex Barzee", 
+        "barzeer@byui.edu",
+        208-496-3768,
+        "../../assets/images/barzeer.jpg", 
+        "null"
+        ),
+    ]
 }
