ae512055fdec4b04607d035fe9f3fd5ddeda7581
Shaira <sil21019@byui.edu>
2023-01-21 20:24:34 +0000

    message edit component updated

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent {
+  currentSender: string = "Shaira";
+  @ViewChild('subject') subject: ElementRef;
+  @ViewChild('msgText') msgText: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  
+  onSendMessage(){
+    const msgSubj = this.subject.nativeElement.value;
+    const msgText = this.msgText.nativeElement.value;
+    const newMessage = new Message(1, msgSubj, msgText, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
 
+  onClear(){
+    this.subject.nativeElement.value = "";
+    this.msgText.nativeElement.value = "";
+  }
 }
