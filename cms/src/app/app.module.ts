87b161350882d9031f3732078060127e6d5274b8
Shaira <sil21019@byui.edu>
2023-01-21 22:52:32 +0000

    forms module added

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
@@ -34,7 +34,8 @@ import { MessageListComponent } from './messages/message-list/message-list.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
