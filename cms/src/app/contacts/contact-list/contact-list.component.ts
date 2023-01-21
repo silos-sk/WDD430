cd332bc03e9b649a41ff5ca0b83e645b6dea7fb5
Shaira <sil21019@byui.edu>
2023-01-21 22:28:30 +0000

    contact detail edits

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -12,14 +12,14 @@ export class ContactListComponent {
       new Contact(1, 
       "R. Kent Jackson", 
       "jacksonk@byui.edu",
-      208-496-3771,
+      "208-496-3771",
       "../../../assets/images/jacksonk.jpg", 
       "null"
       ),
       new Contact(2, 
         "Rex Barzee", 
         "barzeer@byui.edu",
-        208-496-3768,
+        "208-496-3768",
         "../../assets/images/barzeer.jpg", 
         "null"
         ),
--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -3,7 +3,7 @@ export class Contact {
         public id: number, 
         public name: string, 
         public email: string, 
-        public phone: number, 
+        public phone: string, 
         public imageUrl: string, 
         public group: string){}
     
