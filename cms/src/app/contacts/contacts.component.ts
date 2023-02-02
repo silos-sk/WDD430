4f140b1345a995b259beb55c6c7351382635a378
Shaira <sil21019@byui.edu>
2023-02-02 21:51:49 +0000

    contact service list display complete

--- cms/src/app/contacts/contacts.component.ts
+++ cms/src/app/contacts/contacts.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Contact } from './contact.model';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'cms-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  providers: [ContactService]
 })
 export class ContactsComponent {
   selectedContact: Contact;
