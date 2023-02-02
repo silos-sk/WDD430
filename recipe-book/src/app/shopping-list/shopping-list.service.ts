fe287809f46ad4fea0a41dd6961f5207ddd9a6ae
Shaira <sil21019@byui.edu>
2023-02-02 21:11:29 +0000

    ToShoppingList functionality added

--- recipe-book/src/app/shopping-list/shopping-list.service.ts
+++ recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -3,7 +3,7 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService{
     ingredientsChanged = new EventEmitter<Ingredient[]>();
-    
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -17,4 +17,12 @@ export class ShoppingListService{
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
       }
+
+    addIngredients(ingredients: Ingredient[]){
+        // for (let ingredient of ingredients){
+        //     this.addIngredient(ingredient);
+        // }
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
