37d722135d0d3c1538f6bb1de8f39710f3ade8f9
Shaira <sil21019@byui.edu>
2023-02-25 16:03:05 +0000

    working add document

--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-documents-edit',
@@ -8,25 +11,64 @@ import { Document } from '../document.model';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
+  // @ViewChild('f', { static: false }) docForm: NgForm;
+  // subscription: Subscription;
   originalDocument: Document;
   document: Document;
   id: number;
   editMode: boolean = false;
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private documentService: DocumentService, private router: Router, private route: ActivatedRoute){}
 
-  ngOnInit(): void {
-    this.route.params
-    .subscribe(
-      (params: Params) =>{
-        this.id = +params['id'];
-        this.editMode = params['id'] != null;
-        console.log(this.editMode)
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      if (!id) {
+        this.editMode = false;
+        return;
       }
-    )
+      this.originalDocument = this.documentService.getDocument(id);
+
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    })
   }
 
-  onSubmit(){
-    console.log()
-  }
+  // ngOnInit(): void {
+  //   this.route.params
+  //   .subscribe(
+  //     (params: Params) =>{
+  //       this.id = +params['id'];
+  //       this.editMode = params['id'] != null;
+  //       console.log(this.editMode)
+  //     }
+  //   )
+  // }
+
+  onSubmit(form: NgForm) {
+     let id = " ";
+     let name = form.value.name;
+     let url = form.value.url;
+     let description = form.value.description;
+     if (!form.value.description) {
+       description = "Enter Course Description Here";
+     }
+   
+     const newDocument = new Document(id,name,description, url, " ");
+     if (this.editMode == true) {
+       this.documentService.updateDocument(this.originalDocument, newDocument)
+     }
+     else {
+       this.documentService.addDocument(newDocument);
+     }
+    this.router.navigate(['../'], {relativeTo: this.route})
+ }
+
+ onCancel() {
+  this.router.navigate(['../'], {relativeTo: this.route});
+}
+
 }
