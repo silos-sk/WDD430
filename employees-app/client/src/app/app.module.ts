edf37482c873858dc5f65d0da8c26f8b34523bcd
Shaira <sil21019@byui.edu>
2023-03-20 21:48:20 +0000

    base project

--- employees-app/client/src/app/app.module.ts
+++ employees-app/client/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent,
+   EmployeesListComponent,
+   EmployeeFormComponent,
+   AddEmployeeComponent,
+   EditEmployeeComponent
+ ],
+ imports: [
+   BrowserModule,
+   AppRoutingModule,
+   HttpClientModule,
+   ReactiveFormsModule 
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
